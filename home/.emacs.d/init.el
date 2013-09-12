c4604de95624a35dff26a4313cdf09e9b84632be
Steven Farlie <steven.farlie@gmail.com>
2013-09-12 23:00:50 +1000

    bits and pieces of emacs config

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,7 +1,20 @@
-(load-theme 'wombat)
+; Use Marmalade repo for packages
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
 
+; Themes and GUI stuff
+(load-theme 'wombat)
+(custom-set-variables
+   '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+; Local libraries and settings
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (load "better-defaults/better-defaults.el")
 
-(custom-set-variables
-   '(initial-frame-alist (quote ((fullscreen . maximized)))))
+(setq inhibit-splash-screen t) ; no splash screen, sheesh!
+(global-linum-mode 1) ; line numbers please
+(column-number-mode 1) ; column numbers please
+(setq make-backup-files nil) ; stop creating those backup~ files
+(setq auto-save-default nil) ; stop creating those #autosave# files
