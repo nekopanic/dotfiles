0f0ead17a445faa0ff5086b2c8564bb4e0672f6f
Steven Farlie <steven.farlie@gmail.com>
2013-09-12 22:25:29 +1000

    Fullscreen emacs on startup

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,3 +1,6 @@
-(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+(load-theme 'wombat)
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (load "better-defaults/better-defaults.el")
+(load "fullscreen.el")
+(fullscreen)
